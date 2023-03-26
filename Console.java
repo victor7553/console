15c97e03a3b69300311a0ecfa29129e6a2345498
Victor Hugo <69818265+victor7553@users.noreply.github.com>
2023-03-26 19:21:21 -0300

    Add files via upload

---
+++ Console.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Console {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+	System.out.print("Digite o seu nome: ");
+	String nome = entrada.nextLine();
+	
+	System.out.print("Digite o seu sobrenome: ");
+	String sobrenome = entrada.nextLine();
+	
+	System.out.print("Digite a sua idade ");
+	int idade = entrada.nextInt(); // mudar o tipo da variavel
+	
+	System.out.printf("%s %s tem %d anos", nome, sobrenome, idade);
+		
+		entrada.close();
+	}
+
+}
